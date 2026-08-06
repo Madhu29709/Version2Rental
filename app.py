7f0101ffd49f6e18cf7739168f8ec8d85cfc7f85
Madhu29709 <madhu.codes29@gmail.com>
2026-08-06 12:17:34 +0530

    Refactor app.py for rental price estimation

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def get_base64(image_file):
     with open(image_file, "rb") as f:
         return base64.b64encode(f.read()).decode()
 
-bg_image = get_base64("2bg.png")
+bg_image = get_base64("fbg.png")
 
 st.markdown(
     f"""
